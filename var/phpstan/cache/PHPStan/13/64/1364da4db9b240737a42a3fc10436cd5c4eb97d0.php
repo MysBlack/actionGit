b2a1653d22f6caf4ecd11b62d73e973f309801c9
unknown <maryonmouhel@hotmail.fr>
2025-10-28 11:00:15 +0100

    style: apply cs-fixer rules

---
+++ var/phpstan/cache/PHPStan/13/64/1364da4db9b240737a42a3fc10436cd5c4eb97d0.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+// odsl-C:/wamp64/www/dwwm_begles/freshrss/intégration_continue/actionGit/vendor/composer/../symfony/polyfill-intl-normalizer/Resources/stubs
+return \PHPStan\Cache\CacheItem::__set_state(array(
+   'variableKey' => 'v1',
+   'data' => 
+  array (
+    'C:\\wamp64\\www\\dwwm_begles\\freshrss\\intégration_continue\\actionGit\\vendor\\symfony\\polyfill-intl-normalizer\\Resources\\stubs\\Normalizer.php' => 
+    array (
+      0 => 'b44d72072bdc49d6c62ab0216e925fa0146c7a74',
+      1 => 
+      array (
+        0 => 'normalizer',
+      ),
+      2 => 
+      array (
+      ),
+      3 => 
+      array (
+      ),
+    ),
+  ),
+));
---
+++ var/phpstan/cache/PHPStan/5c/64/5c6454bdad5d71cddd1b722f7d52c4e08bd723ec.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+// odsl-C:\wamp64\www\dwwm_begles\freshrss\intégration_continue\actionGit\src
+return \PHPStan\Cache\CacheItem::__set_state(array(
+   'variableKey' => 'v1',
+   'data' => 
+  array (
+    'C:\\wamp64\\www\\dwwm_begles\\freshrss\\intégration_continue\\actionGit\\src\\Math.php' => 
+    array (
+      0 => 'bfa4fdbd0742bbafc37f2617e31ff4a919ecab91',
+      1 => 
+      array (
+        0 => 'app\\math',
+      ),
+      2 => 
+      array (
+        0 => 'app\\add',
+        1 => 'app\\sub',
+        2 => 'app\\mul',
+        3 => 'app\\div',
+      ),
+      3 => 
+      array (
+      ),
+    ),
+  ),
+));
